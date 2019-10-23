717184b46720716c9a76821ff171a55a79929e00
Kevetic <kevetic@outlook.com>
2019-10-22 19:48:56 -0500

    changed hero page to orb

--- assets/js/project.js
+++ assets/js/project.js
@@ -1,5 +1,46 @@
+
+$('#orbContain').hide();
+
+$('#button').on('click', function(any){
+    var fadeIn
+    
+    $('#orbContain').show();//.addClass('zoomIn animated slower');
+    animateCSS('#orb', 'rollIn', () => {
+        $('#orbContain').addClass('scaled')
+
+        animateCSS('#orb', 'heartBeat', () => {
+            $('#orb').addClass('spinning')
+
+            // animateCSS('#orb', 'zoomIn');
+    });
+        
+    })
+})
+
+
+
+function animateCSS(element, animationName, callback) {
+    const node = document.querySelector(element)
+    node.classList.add('animated', animationName)
+
+    function handleAnimationEnd() {
+        node.classList.remove('animated', animationName)
+        node.removeEventListener('animationend', handleAnimationEnd)
+
+        if (typeof callback === 'function') callback()
+    }
+
+    node.addEventListener('animationend', handleAnimationEnd)
+}
+
+
+
 $('.mainPage').hide();
 $('.narutoFadeOut').hide();
+$('#chibiNaru').hide();
+$('#chibiSus').hide();
+$('#shatter').hide();
+
 
 
 $('#hero-search-button').on('click', function(any) {
